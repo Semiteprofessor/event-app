0c57c663676bd051de9c9e0daf3594e407a7076d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-28 17:21:26 +0100

    Commit

---
+++ src/schema/graphql.ts
@@ -0,0 +1,15 @@
+import { makeSchema } from "nexus";
+import * as path from "path";
+import * as types from "./types";
+
+export const schema = makeSchema({
+  types,
+  outputs: {
+    schema: path.join(process.cwd(), "schema.graphql"),
+    typegen: path.join(process.cwd(), "nexus-typegen.ts"),
+  },
+  contextType: {
+    module: path.join(process.cwd(), "src", "context.ts"),
+    export: "Context",
+  },
+});
