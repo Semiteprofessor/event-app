02f275ff1746a45d1f77d14a798303afad1ecf22
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-28 21:14:37 +0100

    Commit

---
+++ src/lib/workers/emailWorker.ts
@@ -0,0 +1,19 @@
+import { Worker, Job } from "bullmq";
+import { redis } from "../redis";
+
+export const emailWorker = new Worker(
+  "emailQueue",
+  async (job: Job) => {
+    console.log("📨 Processing email job:", job.data);
+    await new Promise((res) => setTimeout(res, 1000));
+    console.log(`✅ Email sent to ${job.data.email}`);
+  },
+  { connection: redis }
+);
+
+emailWorker.on("completed", (job) => {
+  console.log(`✅ Job ${job.id} completed`);
+});
+emailWorker.on("failed", (job, err) => {
+  console.error(`❌ Job ${job?.id} failed:`, err);
+});
