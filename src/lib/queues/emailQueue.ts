3e41c2b20d7a71d33d978ce1885753e86153137c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-30 14:20:19 +0100

    Commit

--- src/lib/queues/emailQueue.ts
+++ src/lib/queues/emailQueue.ts
@@ -1,6 +1,19 @@
 import { Queue } from "bullmq";
-import { redis } from "../redis";
+import { redis } from "../redis.js";
 
 export const emailQueue = new Queue("emailQueue", {
   connection: redis,
 });
+
+export async function addEmailJob(data: {
+  to: string;
+  subject: string;
+  body: string;
+}) {
+  await emailQueue.add("sendEmail", data, {
+    attempts: 3,
+    backoff: 5000,
+    removeOnComplete: true,
+    removeOnFail: false,
+  });
+}
