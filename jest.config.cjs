6b17989a8377c4657763d6dc247b2dd53fe57fa7
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-30 10:22:22 +0100

    Commit

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,11 +4,12 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
   moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.(ts|js)$": ["ts-jest", { useESM: true }],
+  },
+  extensionsToTreatAsEsm: [".ts"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{ts,js}", "!src/**/*.d.ts"],
   coverageDirectory: "coverage",
   verbose: true,
-  transform: {
-    "^.+\\.(ts|js)$": ["ts-jest", { useESM: true }],
-  },
 };
