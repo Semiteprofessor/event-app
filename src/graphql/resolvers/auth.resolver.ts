7d230f87e1b02ae0377d0307c4e26f44db5ae1db
Taiwo Olapade <semiteprofessor@gmail.com>
2025-10-06 09:37:26 +0100

    Commit

--- src/graphql/resolvers/auth.resolver.ts
+++ src/graphql/resolvers/auth.resolver.ts
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 const nodemailer = require("nodemailer");
 const { GraphQLError } = require("graphql");
-const { getUserFromToken } = require("../../lib/jwt.js");
+const { getUserFromToken } = require("../../lib/jwt");
 
 const prisma = new PrismaClient();
 
